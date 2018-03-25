6190f5b1f98cdc8ad88ef7ea57c9ecd925896547
bhargav1000 <bhargavsagiraju@gmail.com>
2018-03-25 11:41:12 +0530

    encoding improved

--- encode_img.py
+++ encode_img.py
@@ -2,28 +2,6 @@
 from PIL import Image, ImageFont, ImageDraw
 import textwrap
 
-def decode_image(file_location="images/encoded_sample.png"):
-    """Decodes the hidden message in an image
-
-    file_location: the location of the image file to decode. By default is the provided encoded image in the images folder
-    """
-    encoded_image = Image.open(file_location)
-    red_channel = encoded_image.split()[0]
-
-    x_size = encoded_image.size[0]
-    y_size = encoded_image.size[1]
-
-    decoded_image = Image.new("RGB", encoded_image.size)
-    pixels = decoded_image.load()
-
-    for i in range(x_size):
-        for j in range(y_size):
-            if bin(red_channel.getpixel((i, j)))[-1] == '0':
-                pixels[i, j] = (255, 255, 255)
-            else:
-                pixels[i, j] = (0,0,0)
-    decoded_image.save("images/decoded_image.png")
-
 def write_text(text_to_write, image_size):
     """Writes text to an RGB image. Automatically line wraps
 
@@ -41,11 +19,7 @@ def write_text(text_to_write, image_size):
     return image_text
 
 def encode_image(text_to_encode, template_image="images/hidden.png"):
-    """Encodes a text message into an image
-
-    text_to_encode: the text to encode into the template image
-    template_image: the image to use for encoding. An image is provided by default.
-    """
+    
     template_image = Image.open(template_image)
     red_template = template_image.split()[0]
     green_template = template_image.split()[1]
@@ -76,9 +50,5 @@ def encode_image(text_to_encode, template_image="images/hidden.png"):
     encoded_image.save("images/encoded_image.png")
 
 if __name__ == '__main__':
-    #decode_image()
-    encode_image("""There are multiple ways to hide things within other things, but today we will be working with images. 
-A black and white image (not greyscale) is an easy thing to conceptualize, where a black pixel has a value of 1 
-and a white pixel as a value of 0.
-Color images have three color channels (RGB), with pixel values of 0-255 for each pixel. 
-So a pixel with the value (255,255,255) would be entirely white while (0,0,0) would be black. The upper range is 255 because it is the largest value that can be represented by an 8 bit binary number. Binary is a base-two paradigm, in contrast to decimal which is in base-ten, which means you calculate the value of a binary number by summing the 2s exponent of each place where a 1 appears.""")
+    encodiing_message = input("Enter the encoding message: ")
+    encode_image(encodiing_message)
